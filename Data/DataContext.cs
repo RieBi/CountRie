aaa083959fe753fca074e568e857a0fbfcab133f
RieBi <42877671+RieBi@users.noreply.github.com>
2024-06-30 20:45:05 +0200

    Add DataContext class

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+public class DataContext : DbContext, IDataContext
+{
+    public DbSet<Battle> Battles { get; set; }
+    public DbSet<Character> Characters { get; set; }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<GovernanceType> GovernanceTypes { get; set; }
+    public DbSet<Language> Languages { get; set; }
+    public DbSet<NaturalResource> NaturalResources { get; set; }
+    public DbSet<Religion> Religions { get; set; }
+    public DbSet<Specialty> Specialties { get; set; }
+}
