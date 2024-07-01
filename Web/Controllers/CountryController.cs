e56cbd00cdbd41e68ea99d1b73bb87cf767017cb
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-01 14:38:37 +0200

    Add countries controller and view

---
+++ Web/Controllers/CountryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+public class CountryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        return RedirectToActionPermanent("Index", "Country");
     }
 
     public IActionResult Privacy()
