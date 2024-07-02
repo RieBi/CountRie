4604efd78652d5913a791782a19f7437d1d4679b
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-02 18:48:19 +0200

    Add configuration class for governance types

---
+++ Data/ModelConfigurations/GovernanceTypeConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.ModelConfigurations;
+internal class GovernanceTypeConfiguration : IEntityTypeConfiguration<GovernanceType>
+{
+    public void Configure(EntityTypeBuilder<GovernanceType> builder)
+    {
+        builder.Property(f => f.Name).HasMaxLength(DataConfig.NameLength);
+        builder.Property(f => f.Description).HasMaxLength(DataConfig.DescriptionLength);
+    }
+}
