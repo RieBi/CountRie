c6037bf6743655af1c9349e1810f5c4a1ad98963
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-02 18:07:51 +0200

    Add configuration class for characters

---
+++ Data/ModelConfigurations/CharacterConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.ModelConfigurations;
+internal class CharacterConfiguration : IEntityTypeConfiguration<Character>
+{
+    public void Configure(EntityTypeBuilder<Character> builder)
+    {
+        builder.HasOne(f => f.CurrentCountry);
+        builder.HasOne(f => f.OriginCountry);
+
+        builder.Property(f => f.Name).HasMaxLength(100);
+        builder.Property(f => f.Description).HasMaxLength(5000);
+    }
+}
