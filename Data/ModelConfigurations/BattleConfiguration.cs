903f7c12abe8214263601fc0538abf9671c6c97b
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-02 18:02:40 +0200

    Add configuration class for battles

---
+++ Data/ModelConfigurations/BattleConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.ModelConfigurations;
+internal class BattleConfiguration : IEntityTypeConfiguration<Battle>
+{
+    public void Configure(EntityTypeBuilder<Battle> builder)
+    {
+        builder
+            .HasOne(f => f.WinnerCharacter)
+            .WithMany(f => f.Battles);
+
+        builder
+            .HasOne(f => f.LoserCharacter)
+            .WithMany(f => f.Battles);
+
+        builder.HasOne(f => f.Country);
+
+        builder.Property(f => f.Name).HasMaxLength(100);
+    }
+}
