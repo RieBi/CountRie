0b9e1900e543ac10fae89b0097c1d6f88b386c04
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-03 17:12:37 +0200

    Create CountryListDto class

---
+++ Application/Queries/CountryList/CountryListDto.cs
@@ -0,0 +1,13 @@
+﻿using Data.Models;
+
+namespace Application.Queries.CountryList;
+public class CountryListDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public GovernanceType GovernanceType { get; set; } = default!;
+    public Language Language { get; set; } = default!;
+    public Religion Religion { get; set; } = default!;
+    public string FlagUrl { get; set; } = default!;
+}
