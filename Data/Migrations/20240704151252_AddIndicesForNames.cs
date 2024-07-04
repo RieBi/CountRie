3d1f8e9db98949193a833f7f335b642e26ed31ec
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-04 17:15:19 +0200

    Create migration that adds indices for names of models

---
+++ Data/Migrations/20240704151252_AddIndicesForNames.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndicesForNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Specialties_Name",
+                table: "Specialties",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Religions_Name",
+                table: "Religions",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_Name",
+                table: "Languages",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GovernanceTypes_Name",
+                table: "GovernanceTypes",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_Name",
+                table: "Countries",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Specialties_Name",
+                table: "Specialties");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Religions_Name",
+                table: "Religions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Languages_Name",
+                table: "Languages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GovernanceTypes_Name",
+                table: "GovernanceTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_Name",
+                table: "Countries");
+        }
+    }
+}
