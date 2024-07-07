f0c7bc1314935b07237b26a2a68ee3121a161389
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-07 15:58:38 +0200

    Create dto class for character details

---
+++ Application/Queries/CharacterQueries/CharacterDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿using Data.Models;
+
+namespace Application.Queries.CharacterQueries;
+public class CharacterDetailsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public DateOnly BirthDate { get; set; }
+    public Country OriginCountry { get; set; } = default!;
+    public string PortraitUrl { get; set; } = default!;
+    public int Power { get; set; }
+
+    public ICollection<Battle> Battles { get; set; } = default!;
+}
