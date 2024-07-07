117a77267dd0172a6ddfca5e5049eb888c4acc5a
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-07 13:51:24 +0200

    Add controller and view for character list

---
+++ Web/Controllers/CharacterController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+public class CharacterController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
