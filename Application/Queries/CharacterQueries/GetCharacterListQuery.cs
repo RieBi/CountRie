9f963b01d071fbc4b9abe4f4ef8e26fa323a8501
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-07 15:16:31 +0200

    Create CharacterListQuery/Handler classes

---
+++ Application/Queries/CharacterQueries/GetCharacterListQuery.cs
@@ -0,0 +1,4 @@
+﻿namespace Application.Queries.CharacterQueries;
+internal class GetCharacterListQuery : IRequest<IList<CharacterListDto>>
+{
+}
---
+++ Application/Queries/CharacterQueries/GetCharacterListQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.CharacterQueries;
+internal class GetCharacterListQueryHandler(DataContext context, IMapper mapper) : IRequestHandler<GetCharacterListQuery, IList<CharacterListDto>>
+{
+    private readonly DataContext _context = context;
+    private readonly IMapper _mapper = mapper;
+
+
+    public Task<IList<CharacterListDto>> Handle(GetCharacterListQuery request, CancellationToken cancellationToken)
+    {
+        var characters = _context.Characters
+            .Include(f => f.OriginCountry);
+
+        var characterDtos = _mapper.Map<IList<CharacterListDto>>(characters);
+
+        return Task.FromResult(characterDtos);
+    }
+}
