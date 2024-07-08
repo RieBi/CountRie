abde9a3284419d3e27f415110c6dc5e08a82da26
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-08 13:26:54 +0200

    Create RandomBattleCommand class

---
+++ Application/Commands/BattleCommands/CreateRandomBattleCommand.cs
@@ -0,0 +1,7 @@
+﻿using Data.Models;
+
+namespace Application.Commands.BattleCommands;
+public class CreateRandomBattleCommand(Character character) : IRequest<Unit>
+{
+    public Character Character { get; set; } = character;
+}
