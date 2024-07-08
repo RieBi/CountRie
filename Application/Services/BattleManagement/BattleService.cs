368e9b0f174339c2f79c5f5b4796accfcc1fb40d
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-08 12:36:32 +0200

    Create BattleService class&interface

---
+++ Application/Services/BattleManagement/BattleService.cs
@@ -0,0 +1,25 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.BattleManagement;
+public class BattleService : IBattleService
+{
+    public Battle ExecuteBattle(Character characterA, Character characterB)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Battle ExecuteBattle(Character character)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Character GetRandomOpponent(Character excludedCharacter)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Application/Services/BattleManagement/IBattleService.cs
@@ -0,0 +1,9 @@
+﻿using Data.Models;
+
+namespace Application.Services.BattleManagement;
+public interface IBattleService
+{
+    Battle ExecuteBattle(Character character);
+    Battle ExecuteBattle(Character characterA, Character characterB);
+    Character GetRandomOpponent(Character excludedCharacter);
+}
