762acd1c0c12bfef44aae063b6cc953d63962bf7
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-08 13:34:44 +0200

    Implement RandomBattleCommandHandler

---
+++ Application/Commands/BattleCommands/CreateRandomBattleCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services.BattleManagement;
+
+namespace Application.Commands.BattleCommands;
+public class CreateRandomBattleCommandHandler(IBattleService battleService) : IRequestHandler<CreateRandomBattleCommand, Unit>
+{
+    private readonly IBattleService _battleService = battleService;
+
+    public async Task<Unit> Handle(CreateRandomBattleCommand request, CancellationToken cancellationToken)
+    {
+        await _battleService.ExecuteBattleAsync(request.Character);
+
+        return Unit.Value;
+    }
+}
