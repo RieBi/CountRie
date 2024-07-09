42144dfd9d58d738767e58e1fbb17b818d654f91
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-09 10:53:14 +0200

    Create battles list page with its associated controller action

--- Web/Controllers/BattleController.cs
+++ Web/Controllers/BattleController.cs
@@ -1,17 +1,23 @@
 ﻿using Application.Commands.BattleCommands;
+using Application.Queries.BattleQueries;
+using AutoMapper;
 using Data.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels.Battles;
 
 namespace Web.Controllers;
-public class BattleController(IMediator mediator) : Controller
+public class BattleController(IMediator mediator, IMapper mapper) : Controller
 {
     private readonly IMediator _mediator = mediator;
+    private readonly IMapper _mapper = mapper;
 
-
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return NoContent();
+        var battles = await _mediator.Send(new GetBattleListQuery());
+        var battlesViewModels = _mapper.Map<IList<BattleDetailsViewModel>>(battles);
+
+        return View(battlesViewModels);
     }
 
     [HttpPost]
