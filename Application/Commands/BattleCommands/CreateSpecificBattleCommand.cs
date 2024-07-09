73b8da3e8e5ea26ceadf003603e454cd9caebfc4
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-09 14:42:21 +0200

    Create CreateSpecificBattleCommand/Handler classes

---
+++ Application/Commands/BattleCommands/CreateSpecificBattleCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Commands.BattleCommands;
+public class CreateSpecificBattleCommand(int characterAId, int characterBId) : IRequest<int>
+{
+    public int CharacterAId { get; set; } = characterAId;
+    public int CharacterBId { get; set; } = characterBId;
+}
---
+++ Application/Commands/BattleCommands/CreateSpecificBattleCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿
+using Application.Services.BattleManagement;
+
+namespace Application.Commands.BattleCommands;
+public class CreateSpecificBattleCommandHandler(IBattleService battleService) : IRequestHandler<CreateSpecificBattleCommand, int>
+{
+    private readonly IBattleService _battleService = battleService;
+
+    public async Task<int> Handle(CreateSpecificBattleCommand request, CancellationToken cancellationToken)
+    {
+        var battle = await _battleService.ExecuteBattleAsync(request.CharacterAId, request.CharacterBId);
+
+        return battle.Id;
+    }
+}
