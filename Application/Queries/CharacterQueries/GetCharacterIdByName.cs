e165b2a625120f64d1519a733c66fdd32b7d19a2
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-09 15:14:59 +0200

    Create GetCharacterIdByName Request/Handler

---
+++ Application/Queries/CharacterQueries/GetCharacterIdByName.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.Queries.CharacterQueries;
+public class GetCharacterIdByName(string name) : IRequest<int?>
+{
+    public string Name { get; set; } = name;
+}
---
+++ Application/Queries/CharacterQueries/GetCharacterIdByNameHandler.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Application.Queries.CharacterQueries;
+public class GetCharacterIdByNameHandler(DataContext context) : IRequestHandler<GetCharacterIdByName, int?>
+{
+    private readonly DataContext _context = context;
+
+    public Task<int?> Handle(GetCharacterIdByName request, CancellationToken cancellationToken)
+    {
+        var character = _context.Characters.SingleOrDefault(f => f.Name == request.Name);
+        int? id = character?.Id;
+
+        return Task.FromResult(id);
+    }
+}
