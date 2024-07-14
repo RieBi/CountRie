08568864da1231b92f9242c67970a1972a06a130
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-14 21:33:10 +0200

    Create LongDescriptions migration

---
+++ Data/Migrations/20240714192441_LongDescriptions.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class LongDescriptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Specialties",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5000)",
+                oldMaxLength: 5000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Religions",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5000)",
+                oldMaxLength: 5000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Languages",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5000)",
+                oldMaxLength: 5000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "GovernanceTypes",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5000)",
+                oldMaxLength: 5000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Countries",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5000)",
+                oldMaxLength: 5000);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LongDescription",
+                table: "Countries",
+                type: "character varying(20000)",
+                maxLength: 20000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Characters",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5000)",
+                oldMaxLength: 5000);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LongDescription",
+                table: "Characters",
+                type: "character varying(20000)",
+                maxLength: 20000,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LongDescription",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "LongDescription",
+                table: "Characters");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Specialties",
+                type: "character varying(5000)",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Religions",
+                type: "character varying(5000)",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Languages",
+                type: "character varying(5000)",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "GovernanceTypes",
+                type: "character varying(5000)",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Countries",
+                type: "character varying(5000)",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Characters",
+                type: "character varying(5000)",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
