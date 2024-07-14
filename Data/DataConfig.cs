3c549cb391c9d3cbbf6694d4ee4ca81cd48dfe9c
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-14 20:58:11 +0200

    Update DataConfig to account for long description

--- Data/DataConfig.cs
+++ Data/DataConfig.cs
@@ -2,6 +2,7 @@
 public static class DataConfig
 {
     public static int NameLength { get; } = 100;
-    public static int DescriptionLength { get; } = 5000;
+    public static int DescriptionLength { get; } = 1000;
+    public static int LongDescriptionLength { get; } = 20000;
     public static int UrlLength { get; } = 500;
 }
