2ba434d74874bf47ce5c068e7d22d057ef973957
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-17 11:07:26 +0200

    Create GetNaturalResourceList Query/Handler

---
+++ Application/Queries/NaturalResourceQueries/GetNaturalResourceListQuery.cs
@@ -0,0 +1,6 @@
+﻿using Data.Models;
+
+namespace Application.Queries.NaturalResourceQueries;
+public class GetNaturalResourceListQuery : IRequest<IList<NaturalResource>>
+{
+}
---
+++ Application/Queries/NaturalResourceQueries/GetNaturalResourceListQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Data.Models;
+
+namespace Application.Queries.NaturalResourceQueries;
+public class GetNaturalResourceListQueryHandler(DataContext context) : IRequestHandler<GetNaturalResourceListQuery, IList<NaturalResource>>
+{
+    private readonly DataContext _context = context;
+
+    public Task<IList<NaturalResource>> Handle(GetNaturalResourceListQuery request, CancellationToken cancellationToken)
+    {
+        var resources = _context.NaturalResources;
+
+        IList<NaturalResource> resourceList = resources.ToList();
+
+        return Task.FromResult(resourceList);
+    }
+}
