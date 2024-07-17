e4edc4e4ab2787553b422869c796c6814a43c4e8
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-17 11:13:22 +0200

    Create NaturalResource controller

---
+++ Web/Controllers/NaturalResourceController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Queries.NaturalResourceQueries;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+public class NaturalResourceController(IMediator mediator, IMapper mapper) : Controller
+{
+    private readonly IMediator _mediator = mediator;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<IActionResult> Index()
+    {
+        var resources = await _mediator.Send(new GetNaturalResourceListQuery());
+
+        return View(resources);
+    }
+}
