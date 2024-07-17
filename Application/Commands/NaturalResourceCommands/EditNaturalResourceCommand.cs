331ddfa4f2e3946d71e22bd39502f8e8cc8d75a4
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-17 15:56:11 +0200

    Create EditNaturalResource Command/Handler

---
+++ Application/Commands/NaturalResourceCommands/EditNaturalResourceCommand.cs
@@ -0,0 +1,7 @@
+﻿using Data.Models;
+
+namespace Application.Commands.NaturalResourceCommands;
+public class EditNaturalResourceCommand(NaturalResource naturalResource) : IRequest<Unit>
+{
+    public NaturalResource Resource { get; } = naturalResource;
+}
---
+++ Application/Commands/NaturalResourceCommands/EditNaturalResourceCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Commands.NaturalResourceCommands;
+public class EditNaturalResourceCommandHandler(DataContext context) : IRequestHandler<EditNaturalResourceCommand, Unit>
+{
+    private readonly DataContext _context = context;
+
+    public async Task<Unit> Handle(EditNaturalResourceCommand request, CancellationToken cancellationToken)
+    {
+        var existingResource = await _context.NaturalResources.FindAsync([request.Resource.Id], cancellationToken: cancellationToken);
+
+        if (existingResource is null)
+            return Unit.Value;
+
+        existingResource.Name = request.Resource.Name;
+        existingResource.Description = request.Resource.Description;
+
+        await _context.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
