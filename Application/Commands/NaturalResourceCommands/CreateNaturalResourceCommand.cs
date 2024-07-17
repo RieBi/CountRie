74d1ee4ce340e30a18e3cdefb6f0949386ff592b
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-17 18:48:41 +0200

    Create CreateNaturalResrouce Command/Handler

---
+++ Application/Commands/NaturalResourceCommands/CreateNaturalResourceCommand.cs
@@ -0,0 +1,7 @@
+﻿using Data.Models;
+
+namespace Application.Commands.NaturalResourceCommands;
+public class CreateNaturalResourceCommand(NaturalResource resource) : IRequest<int>
+{
+    public NaturalResource Resource { get; } = resource;
+}
---
+++ Application/Commands/NaturalResourceCommands/CreateNaturalResourceCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Commands.NaturalResourceCommands;
+public class CreateNaturalResourceCommandHandler(DataContext context) : IRequestHandler<CreateNaturalResourceCommand, int>
+{
+    private readonly DataContext _context = context;
+
+    public async Task<int> Handle(CreateNaturalResourceCommand request, CancellationToken cancellationToken)
+    {
+        var newResource = await _context.NaturalResources.AddAsync(request.Resource, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return newResource.Entity.Id;
+    }
+}
