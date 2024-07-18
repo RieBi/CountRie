8180ed1041ab93e10d18147e4744b231e58ead86
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-18 11:29:52 +0200

    Create specialty controller with index action

---
+++ Web/Controllers/SpecialtyController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Queries.SpecialtyQueries;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+public class SpecialtyController(IMediator mediator, IMapper mapper) : Controller
+{
+    private readonly IMediator _mediator = mediator;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<IActionResult> Index()
+    {
+        var specialties = await _mediator.Send(new GetSpecialtyListQuery());
+
+        return View(specialties);
+    }
+}
