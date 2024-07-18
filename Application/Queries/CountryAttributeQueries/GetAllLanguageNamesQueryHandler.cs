c0846883fa3cb8928a0a58cc0e4b2bb983759cfa
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-18 16:34:52 +0200

    Create GetAllLanguageNames Query/Handler

---
+++ Application/Queries/CountryAttributeQueries/GetAllLanguageNamesQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Queries.CountryAttributeQueries;
+public class GetAllLanguageNamesQueryHandler(DataContext context) : IRequestHandler<GetAllLanguageNamesQuery, IList<string>>
+{
+    private readonly DataContext _context = context;
+
+    public Task<IList<string>> Handle(GetAllLanguageNamesQuery request, CancellationToken cancellationToken)
+    {
+        IList<string> result = [.. _context.Languages.Select(f => f.Name)];
+
+        return Task.FromResult(result);
+    }
+}
