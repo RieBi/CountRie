f1b979276d324b621a6b63ddf14dae7f36fad364
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-18 11:32:24 +0200

    Add Create action to specialty controller

--- Web/Controllers/SpecialtyController.cs
+++ Web/Controllers/SpecialtyController.cs
@@ -1,7 +1,10 @@
-﻿using Application.Queries.SpecialtyQueries;
+﻿using Application.Commands.SpecialtyCommands;
+using Application.Queries.SpecialtyQueries;
 using AutoMapper;
+using Data.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels;
 
 namespace Web.Controllers;
 public class SpecialtyController(IMediator mediator, IMapper mapper) : Controller
@@ -14,5 +17,17 @@ public class SpecialtyController(IMediator mediator, IMapper mapper) : Controlle
         var specialties = await _mediator.Send(new GetSpecialtyListQuery());
 
         return View(specialties);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromForm] ShortInfoViewModel specialty)
+    {
+        if (!ModelState.IsValid)
+            return this.RedirectBack();
+
+        var specialtyModel = _mapper.Map<Specialty>(specialty);
+        await _mediator.Send(new CreateSpecialtyCommand(specialtyModel));
+
+        return this.RedirectBack();
     }
 }
