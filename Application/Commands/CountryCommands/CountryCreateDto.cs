304ecaf3c2f808931977a1b9b10eaedcc7ac8bf3
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-18 17:09:11 +0200

    Create CountryCreateDto class

---
+++ Application/Commands/CountryCommands/CountryCreateDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Commands.CountryCommands;
+public class CountryCreateDto
+{
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string LongDescription { get; set; } = default!;
+    public string Capital { get; set; } = default!;
+    public long Population { get; set; }
+    public long Area { get; set; }
+    public int IndependenceYear { get; set; }
+    public string GovernanceTypeName { get; set; } = default!;
+    public string LanguageName { get; set; } = default!;
+    public string ReligionName { get; set; } = default!;
+    public string FlagUrl { get; set; } = default!;
+
+    public IList<string> NaturalResourceNames { get; set; } = default!;
+    public IList<string> SpecialtyNames { get; set; } = default!;
+}
