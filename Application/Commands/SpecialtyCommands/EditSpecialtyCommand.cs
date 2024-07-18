cc5cea1231982a6337df5a6d6547862f606433c5
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-18 11:21:50 +0200

    Create EditSpecialty Command/Handler

---
+++ Application/Commands/SpecialtyCommands/EditSpecialtyCommand.cs
@@ -0,0 +1,7 @@
+﻿using Data.Models;
+
+namespace Application.Commands.SpecialtyCommands;
+public class EditSpecialtyCommand(Specialty specialty) : IRequest<Unit>
+{
+    public Specialty Specialty { get; } = specialty;
+}
---
+++ Application/Commands/SpecialtyCommands/EditSpecialtyCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Commands.SpecialtyCommands;
+public class EditSpecialtyCommandHandler(DataContext context) : IRequestHandler<EditSpecialtyCommand, Unit>
+{
+    private readonly DataContext _context = context;
+
+    public async Task<Unit> Handle(EditSpecialtyCommand request, CancellationToken cancellationToken)
+    {
+        var existingSpecialty = await _context.Specialties.FindAsync([request.Specialty.Id], cancellationToken: cancellationToken);
+
+        if (existingSpecialty is null)
+            return Unit.Value;
+
+        existingSpecialty.Name = request.Specialty.Name;
+        existingSpecialty.Description = request.Specialty.Description;
+
+        await _context.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
