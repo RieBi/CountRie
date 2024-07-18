04766d2de1e8c5f14b1630f84ecdd2151623b515
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-18 11:13:00 +0200

    Create CreateSpecialty Command/Handler

---
+++ Application/Commands/SpecialtyCommands/CreateSpecialtyCommand.cs
@@ -0,0 +1,7 @@
+﻿using Data.Models;
+
+namespace Application.Commands.SpecialtyCommands;
+public class CreateSpecialtyCommand(Specialty specialty) : IRequest<int>
+{
+    public Specialty Specialty { get; } = specialty;
+}
---
+++ Application/Commands/SpecialtyCommands/CreateSpecialtyCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Commands.SpecialtyCommands;
+public class CreateSpecialtyCommandHandler(DataContext context) : IRequestHandler<CreateSpecialtyCommand, int>
+{
+    private readonly DataContext _context = context;
+
+    public async Task<int> Handle(CreateSpecialtyCommand request, CancellationToken cancellationToken)
+    {
+        await _context.Specialties.AddAsync(request.Specialty, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return request.Specialty.Id;
+    }
+}
