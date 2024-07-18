d12afbcc5b4822fc4ed60c74cfb109f6d5e252de
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-18 11:18:10 +0200

    Create DeleteSpecialty Command/Handler

---
+++ Application/Commands/SpecialtyCommands/DeleteSpecialtyCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.Commands.SpecialtyCommands;
+public class DeleteSpecialtyCommand(int id) : IRequest<Unit>
+{
+    public int Id { get; } = id;
+}
---
+++ Application/Commands/SpecialtyCommands/DeleteSpecialtyCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Commands.SpecialtyCommands;
+public class DeleteSpecialtyCommandHandler(DataContext context) : IRequestHandler<DeleteSpecialtyCommand, Unit>
+{
+    private readonly DataContext _context = context;
+
+    public async Task<Unit> Handle(DeleteSpecialtyCommand request, CancellationToken cancellationToken)
+    {
+        var specialty = await _context.Specialties.FindAsync([request.Id], cancellationToken: cancellationToken);
+
+        if (specialty is null)
+            return Unit.Value;
+
+        _context.Specialties.Remove(specialty);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
