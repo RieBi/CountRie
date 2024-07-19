967b1dbdb0990a2c9f647501d26636449d85602f
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-19 17:35:57 +0200

    Create EditCharacter Command/Handler

---
+++ Application/Commands/CharacterCommands/EditCharacterCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Commands.CharacterCommands;
+public class EditCharacterCommand(int id, CharacterCreateDto characterCreateDto) : IRequest<Unit>
+{
+    public int Id { get; } = id;
+    public CharacterCreateDto characterCreateDto { get; } = characterCreateDto;
+}
---
+++ Application/Commands/CharacterCommands/EditCharacterCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Converters;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Commands.CharacterCommands;
+public class EditCharacterCommandHandler(DataContext context) : IRequestHandler<EditCharacterCommand, Unit>
+{
+    private readonly DataContext _context = context;
+
+    public async Task<Unit> Handle(EditCharacterCommand request, CancellationToken cancellationToken)
+    {
+        var character = await _context.Characters
+            .Where(f => f.Id == request.Id)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (character is null)
+            return Unit.Value;
+
+        character = await new CharacterConverter(_context)
+            .TryConvertFromDto(request.characterCreateDto, cancellationToken, character);
+
+        if (character is null)
+            return Unit.Value;
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
