cb2c8e94dc4b582f7ceb60d5b919d0f6063cc14a
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-19 14:15:00 +0200

    Create CharacterConvertere

---
+++ Application/Converters/CharacterConverter.cs
@@ -0,0 +1,36 @@
+﻿using Application.Commands.CharacterCommands;
+using Application.Commands.CountryCommands;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Application.Converters;
+internal class CharacterConverter(DataContext context)
+{
+    private readonly DataContext _context = context;
+
+    public async Task<Character?> TryConvertFromDto(CharacterCreateDto dto, CancellationToken cancellationToken, Character? character = null)
+    {
+        var country = await _context.Countries
+            .Where(f => f.Name == dto.OriginCountryName)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (country is null)
+            return null;
+
+        character ??= new();
+
+        character.Name = dto.Name;
+        character.Description = dto.Description;
+        character.LongDescription = dto.LongDescription;
+        character.BirthDate = dto.BirthDate;
+        character.OriginCountry = country;
+        character.PortraitUrl = dto.PortraitUrl;
+        character.Power = dto.Power;
+
+        return character;
+    }
+}
