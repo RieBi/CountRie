f0257fa8262889edc91a5f13acf099d2a4d8c912
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-19 12:59:08 +0200

    Create DeleteCountry Command/Handler

---
+++ Application/Commands/CountryCommands/DeleteCountryCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.Commands.CountryCommands;
+public class DeleteCountryCommand(int id) : IRequest<Unit>
+{
+    public int Id { get; } = id;
+}
---
+++ Application/Commands/CountryCommands/DeleteCountryCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Commands.CountryCommands;
+public class DeleteCountryCommandHandler(DataContext context) : IRequestHandler<DeleteCountryCommand, Unit>
+{
+    private readonly DataContext _context = context;
+
+    public async Task<Unit> Handle(DeleteCountryCommand request, CancellationToken cancellationToken)
+    {
+        var countryToDelete = await _context.Countries.FindAsync([request.Id], cancellationToken: cancellationToken);
+
+        if (countryToDelete is null)
+            return Unit.Value;
+
+        _context.Remove(countryToDelete);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
