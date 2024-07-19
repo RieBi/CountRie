c5981dc92dc35cccdc6a0f04351d1f8c618b7c56
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-19 13:38:06 +0200

    Create CharacterCreateDto

---
+++ Application/Commands/CharacterCommands/CharacterCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using Data.Models;
+
+namespace Application.Commands.CharacterCommands;
+internal class CharacterCreateDto
+{
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string LongDescription { get; set; } = default!;
+    public DateOnly BirthDate { get; set; }
+    public Country OriginCountry { get; set; } = default!;
+    public string PortraitUrl { get; set; } = default!;
+    public int Power { get; set; }
+}
