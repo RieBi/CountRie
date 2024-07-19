cf43fc1de8a21d9964143f3b0d6b87397a78f23b
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-19 16:36:17 +0200

    Add a script for making textareas resizable

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -4,3 +4,12 @@
 // Write your JavaScript code.
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.querySelectorAll('textarea').forEach(area => {
+    area.style.overflowY = "hidden";
+    area.style.height = 0;
+    area.oninput = function () {
+        area.style.height = 0;
+        area.style.height = (this.scrollHeight) + "px";
+    }
+});
