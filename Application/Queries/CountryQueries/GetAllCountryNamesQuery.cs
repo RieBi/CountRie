beda85da5b32902c5b73c42c14522bc4d35ef7c7
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-19 14:27:35 +0200

    Create GetAllCountryNames Query/Handler

---
+++ Application/Queries/CountryQueries/GetAllCountryNamesQuery.cs
@@ -0,0 +1,4 @@
+﻿namespace Application.Queries.CountryQueries;
+public class GetAllCountryNamesQuery : IRequest<IList<string>>
+{
+}
---
+++ Application/Queries/CountryQueries/GetAllCountryNamesQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Queries.CountryQueries;
+public class GetAllCountryNamesQueryHandler(DataContext context) : IRequestHandler<GetAllCountryNamesQuery, IList<string>>
+{
+    private readonly DataContext _context = context;
+
+    public Task<IList<string>> Handle(GetAllCountryNamesQuery request, CancellationToken cancellationToken)
+    {
+        IList<string> result = [.. _context.Countries.Select(f => f.Name)];
+
+        return Task.FromResult(result);
+    }
+}
