1bb3b6e2f793487208b0dde4c5822127ed06a262
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-19 14:17:42 +0200

    Create CreateCharacter Command/Handler

---
+++ Application/Commands/CharacterCommands/CreateCharacterCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.Commands.CharacterCommands;
+public class CreateCharacterCommand(CharacterCreateDto characterCreateDto) : IRequest<int>
+{
+    public CharacterCreateDto CharacterCreateDto { get; } = characterCreateDto;
+}
---
+++ Application/Commands/CharacterCommands/CreateCharacterCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Converters;
+
+namespace Application.Commands.CharacterCommands;
+public class CreateCharacterCommandHandler(DataContext context) : IRequestHandler<CreateCharacterCommand, int>
+{
+    private readonly DataContext _context = context;
+
+    public async Task<int> Handle(CreateCharacterCommand request, CancellationToken cancellationToken)
+    {
+        var character = await new CharacterConverter(_context)
+            .TryConvertFromDto(request.CharacterCreateDto, cancellationToken);
+
+        if (character is null)
+            return -1;
+
+        await _context.Characters.AddAsync(character, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return character.Id;
+    }
+}
