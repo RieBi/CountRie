c3da33fc5f0737f5da93fef609db7585b68f78a1
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-19 15:21:17 +0200

    Add validation for CharacterCreateDto

--- Application/Commands/CharacterCommands/CharacterCreateDto.cs
+++ Application/Commands/CharacterCommands/CharacterCreateDto.cs
@@ -1,12 +1,17 @@
-﻿using Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Application.Commands.CharacterCommands;
 public class CharacterCreateDto
 {
+    [MaxLength(DataConfig.NameLength)]
     public string Name { get; set; } = default!;
+    [MaxLength(DataConfig.DescriptionLength)]
     public string Description { get; set; } = default!;
+    [MaxLength(DataConfig.LongDescriptionLength)]
     public string LongDescription { get; set; } = default!;
     public DateOnly BirthDate { get; set; }
+    [MaxLength(DataConfig.NameLength)]
     public string OriginCountryName { get; set; } = default!;
+    [MaxLength(DataConfig.UrlLength)]
     public string PortraitUrl { get; set; } = default!;
 }
