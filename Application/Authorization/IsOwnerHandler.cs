1c7f73ceb9dcccc79fa69c7f8fccbb8d51e1e0da
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-19 20:34:42 +0200

    Create IsOwnerHandler authorization handler

---
+++ Application/Authorization/IsOwnerHandler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Authorization;
+internal class IsOwnerHandler(UserManager<IdentityUser> userManager, DataContext context) : AuthorizationHandler<OperationAuthorizationRequirement, int>
+{
+    private readonly UserManager<IdentityUser> _userManager = userManager;
+    private readonly DataContext _context = context;
+
+    protected async override Task HandleRequirementAsync(AuthorizationHandlerContext context, OperationAuthorizationRequirement requirement, int resource)
+    {
+        var user = await _userManager.GetUserAsync(context.User);
+
+        if (user is null)
+            return;
+
+        var email = await _userManager.GetEmailAsync(user);
+        if (email is null)
+            return;
+
+        email = email.ToLowerInvariant();
+
+        if (requirement.Name == Requirements.Character.Name)
+        {
+            var ownerMail = await _context.Characters
+                .Where(f => f.Id == resource)
+                .Select(f => f.OwnerEmail)
+                .SingleOrDefaultAsync();
+
+            if (ownerMail is null)
+                return;
+
+            if (ownerMail == email)
+                context.Succeed(requirement);
+        }
+        else if (requirement.Name == Requirements.Country.Name)
+        {
+            var ownerMail = await _context.Countries
+                .Where(f => f.Id == resource)
+                .Select(f => f.OwnerEmail)
+                .SingleOrDefaultAsync();
+
+            if (ownerMail is null)
+                return;
+
+            if (ownerMail == email)
+                context.Succeed(requirement);
+
+        }
+    }
+}
---
+++ Application/Authorization/Requirements.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace Application.Authorization;
+public static class Requirements
+{
+    public static OperationAuthorizationRequirement Country { get; } 
+        = new() { Name = "Country" };
+
+    public static OperationAuthorizationRequirement Character { get; }
+        = new() { Name = "Character" };
+
+}
