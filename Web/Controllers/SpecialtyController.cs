a63ef9c18475ba52540b56e5e4f191c392142ed7
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-20 08:13:42 +0200

    Specialty actions are now authorized

--- Web/Controllers/SpecialtyController.cs
+++ Web/Controllers/SpecialtyController.cs
@@ -3,6 +3,7 @@ using Application.Queries.SpecialtyQueries;
 using AutoMapper;
 using Data.Models;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Web.ViewModels;
 
@@ -20,6 +21,7 @@ public class SpecialtyController(IMediator mediator, IMapper mapper) : Controlle
     }
 
     [HttpPost]
+    [Authorize(Roles = "admin")]
     public async Task<IActionResult> Create([FromForm] ShortInfoViewModel specialty)
     {
         if (!ModelState.IsValid)
@@ -32,6 +34,7 @@ public class SpecialtyController(IMediator mediator, IMapper mapper) : Controlle
     }
 
     [HttpPost]
+    [Authorize(Roles = "admin")]
     public async Task<IActionResult> Edit([FromForm] ShortInfoViewModel specialty)
     {
         if (!ModelState.IsValid)
@@ -44,6 +47,7 @@ public class SpecialtyController(IMediator mediator, IMapper mapper) : Controlle
     }
 
     [HttpPost]
+    [Authorize(Roles = "admin")]
     public async Task<IActionResult> Delete(int id)
     {
         if (!ModelState.IsValid)
