509dabba6260c9169b3addecbb61cbda1c4b36cd
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-21 19:59:27 +0200

    Create UserCreatedViewModel

---
+++ Web/ViewModels/Personal/UserCreatedViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Application.Queries.CharacterQueries;
+using Application.Queries.CountryQueries;
+
+namespace Web.ViewModels.Personal;
+
+public class UserCreatedViewModel
+{
+    public required IList<CountryListDto> UserCountries { get; set; } = [];
+    public required IList<CharacterListDto> UserCharacters { get; set; } = [];
+}
