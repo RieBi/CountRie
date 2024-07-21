a87be0ee3092afc456a3c7601042ad72a0c0bdad
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-21 19:57:18 +0200

    Add base "Your Creations" page and controller

---
+++ Web/Controllers/PersonalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+public class PersonalController : Controller
+{
+    [Authorize]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
