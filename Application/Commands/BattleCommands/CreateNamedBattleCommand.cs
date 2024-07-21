3b60025f5d1ecbe50701116ab4980466de0aa22e
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-21 18:45:16 +0200

    Create CreateNamedBattle Command/Handler

---
+++ Application/Commands/BattleCommands/CreateNamedBattleCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Commands.BattleCommands;
+public class CreateNamedBattleCommand(int characterId1, int characterId2, string battleName) : IRequest<int>
+{
+    public int CharacterId1 { get; } = characterId1;
+    public int CharacterId2 { get; } = characterId2;
+    public string BattleName { get; } = battleName;
+}
---
+++ Application/Commands/BattleCommands/CreateNamedBattleCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Application.Services.BattleManagement;
+
+namespace Application.Commands.BattleCommands;
+public class CreateNamedBattleCommandHandler(IBattleService battleService) : IRequestHandler<CreateNamedBattleCommand, int>
+{
+    private readonly IBattleService _battleService = battleService;
+
+    public async Task<int> Handle(CreateNamedBattleCommand request, CancellationToken cancellationToken)
+    {
+        var battle = await _battleService.ExecuteBattleAsync(request.CharacterId1, request.CharacterId2, request.BattleName);
+
+        if (battle is null)
+            return -1;
+
+        return battle.Id;
+    }
+}
