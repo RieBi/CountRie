7036d75a55073408b7b6f50a2dfac2d95572dafe
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-21 20:13:24 +0200

    Create GetUserCreatedEntities Query/Handler

---
+++ Application/Queries/PersonalQueries/GetUserCreatedEntitiesQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using Application.Queries.CharacterQueries;
+using Application.Queries.CountryQueries;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Queries.PersonalQueries;
+public class GetUserCreatedEntitiesQueryHandler(UserManager<IdentityUser> userManager, DataContext context, IMapper mapper) : IRequestHandler<GetUserCreatedEntitiesQuery, UserCreatedDto>
+{
+    private readonly UserManager<IdentityUser> _userManager = userManager;
+    private readonly DataContext _context = context;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<UserCreatedDto> Handle(GetUserCreatedEntitiesQuery request, CancellationToken cancellationToken)
+    {
+        var user = await _userManager.GetUserAsync(request.User);
+        var dto = new UserCreatedDto();
+
+        if (user is null)
+            return dto;
+
+        var email = await _userManager.GetEmailAsync(user);
+        if (email is null)
+            return dto;
+
+        var userCountries = await _context.Countries
+            .Where(f => f.OwnerEmail == email)
+            .ProjectTo<CountryListDto>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        var userCharacters = await _context.Countries
+            .Where(f => f.OwnerEmail == email)
+            .ProjectTo<CharacterListDto>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        dto.UserCountries = userCountries;
+        dto.UserCharacters = userCharacters;
+
+        return dto;
+    }
+}
---
+++ Application/Queries/PersonalQueries/UserCreatedDto.cs
@@ -0,0 +1,9 @@
+﻿using Application.Queries.CharacterQueries;
+using Application.Queries.CountryQueries;
+
+namespace Application.Queries.PersonalQueries;
+public class UserCreatedDto
+{
+    public IList<CountryListDto> UserCountries { get; set; } = [];
+    public IList<CharacterListDto> UserCharacters { get; set; } = [];
+}
